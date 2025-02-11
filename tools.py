999dae32fad5be9d57acbd909bfb6ce691f328cd
liad <liad@gmail.com>
2025-02-11 16:15:07 +0200

    updated

--- tools.py
+++ tools.py
@@ -1,56 +1,18 @@
-from log import setup_logging
+import logging
 
-# logger
-logger = setup_logging()
+logger = logging.getLogger("myapp")
+servers = {"nginX": True, "DoCkEr": False}
 
-# servers list and status
-servers_dict = {
-    
-    "Nginx": "Running",
-    "Docker": "Not Running",
-    "Terraform": "Running",
-    "Kubernetes": "Running",
-    "AWS": "Not Running"
-}
-
-# converting servers name to lower case
-new_servers_dict = { key.strip().lower() : value for key, value in servers_dict.items()}
-
-
-
-# asking for user input until completion
-
-server_name = input("Please Enter server name: ")
-
-def check_server(server_name):
-    server_name = server_name.strip().lower()
-    logger.info(f"The user chose: {server_name}")
+def get_server_status(server_name: str) -> bool:
+    lowercase_servers = {key.strip().lower(): value for key, value in servers.items()}
     try:
-        print(f"{server_name} is {new_servers_dict[server_name]}")
-
+        return lowercase_servers[server_name]
     except KeyError:
-            print("Server is not recognized.")
-            logger.error("Invalid Input from user")
+        logger.error(f"The server name {server_name} does not exist")
 
-            
-            
-            
-#---------------------------------------------------------------------#
-#----------------------- Another main function -----------------------#
-#---------------------------------------------------------------------#
 
-# servers = {"nginx": True, "docker" : False}
-
-# def get_server_status(server_name: str) -> bool:
-    
-#     try:
-#         return servers[server_name]
-#     except KeyError:
-#         logger.error("This server does not exist")
-#     return False 
-        
-# while True:
-#     server_name = input("Please Enter server name: ")
-#     server_name = server_name.lower()
-#     status = get_server_status(server_name)
-#     logger.info(f"Server status is {str(status)}")
+def check_servers_from_terminal():
+    while True:
+        server_name = input("Enter server name: ").strip().lower()
+        status = get_server_status(server_name)
+        logger.info(f"Server {server_name} status is: {status}")
