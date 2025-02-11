048eff7c68f57e80380de602206cbbeb22441d71
liad <liad@gmail.com>
2025-02-11 12:12:32 +0200

    updated main with fastAPI

--- main.py
+++ main.py
@@ -1,62 +1,28 @@
-from log import setup_logging
 
-# logger
-logger = setup_logging()
+from dataclasses import dataclass
+from fastapi import FastAPI
+import httpx
 
-# servers list and status
-servers_dict = {
-    
-    "Nginx": "Running",
-    "Docker": "Not Running",
-    "Terraform": "Running",
-    "Kubernetes": "Running",
-    "AWS": "Not Running"
-}
-
-# converting servers name to lower case
-new_servers_dict = { key.strip().lower() : value for key, value in servers_dict.items()}
-
-# asking for user input until completion
-while True:
-    user_input = input("Please Enter server name: ")
-    user_input = user_input.strip().lower()
-    logger.info(f"The user chose: {user_input}")
-    try:
-        print(f"{user_input} is {new_servers_dict[user_input]}")
-        break
-    except KeyError:
-            print("Server is not recognized. type 'quit' to leave or 'list' for servers list")
-            logger.error("Invalid Input from user")
-    
-    if (user_input == "quit"):
-            print("Okay, Bye")
-            logger.info(f"The user chose to quit")
-            break
-    if (user_input == "list"):
-        print("Current Servers are: ")
-        logger.info(f"The user chose to view all servers")
-        for item in new_servers_dict:
-            print(item)
-        
+app = FastAPI()
 
 
+@dataclass
+class User:
+    name: str
+    email: str
 
-#---------------------------------------------------------------------#
-#----------------------- Another main function -----------------------#
-#---------------------------------------------------------------------#
 
-# servers = {"nginx": True, "docker" : False}
+@app.get("/")
+def hello_world():
+    "This is our main function"
+    return "Hello World!"
 
-# def get_server_status(server_name: str) -> bool:
-    
-#     try:
-#         return servers[server_name]
-#     except KeyError:
-#         logger.error("This server does not exist")
-#     return False 
-        
-# while True:
-#     server_name = input("Please Enter server name: ")
-#     server_name = server_name.lower()
-#     status = get_server_status(server_name)
-#     logger.info(f"Server status is {str(status)}")
+@app.get("/users")
+def get_users() -> list[User]:
+    response = httpx.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    return users
+
+@app.post("/users")
+def create_user(new_user: User) -> bool:
+    return True
