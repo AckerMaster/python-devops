481c78440f76bb72114c392135313cecb40f0b26
liad <liad@gmail.com>
2025-02-11 12:11:34 +0200

    demo

---
+++ pipdemo.py
@@ -0,0 +1,11 @@
+import httpx
+import json
+from pprint import pprint
+
+response = httpx.get("https://jsonplaceholder.typicode.com/users")
+users = response.json()
+
+for user in users:
+    print(user["name"])
+    
+pprint(users[0])
