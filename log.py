0ec827c28d345d5b528726c339558d947354ef84
liad <liad@gmail.com>
2025-02-12 22:34:43 +0200

    updated file structure for better readability

--- log.py
+++ log.py
@@ -3,6 +3,10 @@ import sys
 import logging
 import json
 
+# logs file
+LOGS_FILE = "log.txt"
+
+# a costum JSON format
 class JsonFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         log = {
@@ -12,22 +16,29 @@ class JsonFormatter(logging.Formatter):
         }
         return json.dumps(log)
 
+# function that returns a logger that logs to both terminal and a file
 def setup_logging():
+    
+    # environment variables
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "DEBUG")
     LOG_FORMAT = os.environ.get("LOG_FORMAT", "TEXT")
 
     logger = logging.getLogger("myapp")
     logger.setLevel(LOG_LEVEL)
     stdout_handler = logging.StreamHandler(sys.stdout)
-    file_handler = logging.FileHandler("log.txt")
+    
+    file_handler = logging.FileHandler(LOGS_FILE)
 
-    if LOG_FORMAT == "JSON":
+    if (LOG_FORMAT == "JSON"):
         formatter = JsonFormatter()
     else:
-        formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(module)s:%(lineno)d:%(funcName)s:%(message)s")
+        text_format = "%(asctime)s:%(name)s:%(levelname)s:%(module)s:%(lineno)d:%(funcName)s:%(message)s"
+        formatter = logging.Formatter(text_format)
         
     stdout_handler.setFormatter(formatter)
     logger.addHandler(stdout_handler)
+    
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
+    
     return logger
